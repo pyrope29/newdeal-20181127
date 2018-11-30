600c422eebf387b5a3c95a61a884e01abb80323e
pyrope29 <pyrope29@naver.com>
2018-11-30 09:07:21 +0900

    assignment

---
+++ newdeal-project-04/src/main/java/myPackage/MyApp.java
@@ -0,0 +1,35 @@
+package myPackage;
+
+import java.util.Date;
+import java.util.Scanner;
+
+public class MyApp {
+
+  final static int LENGTH = 10;
+
+  int[] no = new int[LENGTH];
+  String[] name = new String[LENGTH];
+  String[] contents = new String[LENGTH];
+  Date[] startDate = new Date[LENGTH];
+  Date[] endDate = new Date[LENGTH];
+  int[] wholeTime = new int[LENGTH];
+  int[] dateTime= new int[LENGTH];
+
+  Scanner scan = new Scanner(System.in);
+  
+  MyLesson myLessons = new MyLesson();
+
+  public static void main(String[] args) {
+    
+  int i=0;
+    
+    while(i<LENGTH) {
+      System.out.println("번호? ");
+      
+   
+      
+    }
+
+  }
+
+}
---
+++ newdeal-project-04/src/main/java/myPackage/MyLesson.java
@@ -0,0 +1,73 @@
+package myPackage;
+
+import java.util.Date;
+
+public class MyLesson {
+
+  private int no;
+  String name;
+  String contents;
+  Date startDate;
+  Date endDate;
+  int wholeTime;
+  int dateTime;
+
+  public int getNo() {
+    return no;
+  }
+
+  public void setNo(int no) {
+    this.no = no;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getContents() {
+    return contents;
+  }
+
+  public void setContents(String contents) {
+    this.contents = contents;
+  }
+
+  public Date getStartDate() {
+    return startDate;
+  }
+
+  public void setStartDate(Date startDate) {
+    this.startDate = startDate;
+  }
+
+  public Date getEndDate() {
+    return endDate;
+  }
+
+  public void setEndDate(Date endDate) {
+    this.endDate = endDate;
+  }
+
+  public int getWholeTime() {
+    return wholeTime;
+  }
+
+  public void setWholeTime(int wholeTime) {
+    this.wholeTime = wholeTime;
+  }
+
+  public int getDateTime() {
+    return dateTime;
+  }
+
+  public void setDateTime(int dateTime) {
+    this.dateTime = dateTime;
+  }
+
+
+
+}
